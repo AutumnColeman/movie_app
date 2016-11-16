7be590e29e29e0e863588db64f13c8739cd53bcc
Autumn Coleman <autumn.eve.coleman@gmail.com>
2016-11-16 09:13:10 -0500

    Added files for movie app

---
+++ movieapp.js
@@ -0,0 +1,20 @@
+var app = angular.module("MovieApp", []);
+app.factory('MovieService', function($http) {
+  var service = {};
+  var API_KEY = '8fa482ca4a8b8964d4f857eee886e8e9';
+  service.nowPlaying = function() {
+    var url = 'http://api.themoviedb.org/3/movie/now_playing';
+    return $http({
+      method: 'GET',
+      url: url,
+      params: { api_key: API_KEY }
+    });
+  };
+  return service;
+});
+
+app.controller('MainController', function(MovieService) {
+  MovieService.nowPlaying().success(function(movieResults) {
+    console.log('Movie results', movieResults);
+  });
+});
